62d60a6cb59a3c341abec1d74bc99612c6a398bd
Marcelo "Ataxexe" Guimarães <ataxexe@backpackcloud.com>
2024-06-17 11:50:52 +0200

    refactor InputValue class

--- src/main/java/com/backpackcloud/text/InputValue.java
+++ src/main/java/com/backpackcloud/text/InputValue.java
@@ -17,64 +17,64 @@ public interface InputValue extends Supplier<String> {
   @Override
   String get();
 
-  default String asString() {
+  default Optional<String> text() {
     String value = get();
-    return value == null ? "" : value;
+    return value == null || value.isEmpty() ? Optional.empty() : Optional.of(value);
   }
 
-  default String or(String defaultValue) {
-    String value = asString();
-    return value.isEmpty() ? defaultValue : value;
+  default <T> Optional<T> map(Function<String, T> mapper) {
+    return text().map(mapper);
   }
 
-  default <R> Optional<R> map(Function<String, R> function) {
-    String value = asString();
-    return value.isEmpty() ? Optional.empty() : Optional.ofNullable(function.apply(asString()));
+  default Optional<Integer> integer() {
+    try {
+      return map(Integer::parseInt);
+    } catch (NumberFormatException e) {
+      return Optional.empty();
+    }
   }
 
-  default int asInt() {
-    return Integer.parseInt(asString());
+  default Optional<Long> number() {
+    try {
+      return map(Long::parseLong);
+    } catch (NumberFormatException e) {
+      return Optional.empty();
+    }
   }
 
-  default int or(int defaultValue) {
-    String value = asString();
-    return value.isEmpty() ? defaultValue : asInt();
+  default Optional<Double> decimal() {
+    try {
+      return map(Double::parseDouble);
+    } catch (NumberFormatException e) {
+      return Optional.empty();
+    }
   }
 
-  default long asLong() {
-    return Long.parseLong(asString());
+  default Optional<Boolean> bool() {
+    return map(Boolean::parseBoolean);
   }
 
-  default long or(long defaultValue) {
-    String value = asString();
-    return value.isEmpty() ? defaultValue : asLong();
+  default <T> Optional<T> temporal(String pattern, TemporalQuery<T> query) {
+    return map(input -> DateTimeFormatter.ofPattern(pattern).parse(input, query));
   }
 
-  default boolean asBoolean() {
-    return Boolean.parseBoolean(asString());
-  }
-
-  default boolean or(boolean defaultValue) {
-    String value = asString();
-    return value.isEmpty() ? defaultValue : asBoolean();
-  }
-
-  default <T extends Enum<T>> T asEnum(Class<T> enumType) {
-    return Enum.valueOf(enumType, asString().toUpperCase().replaceAll("([- .])", "_"));
-  }
-
-  default <T extends Enum<T>> T or(T defaultValue) {
-    String value = asString();
-    return value.isEmpty() ? defaultValue : asEnum(defaultValue.getDeclaringClass());
-  }
-
-  default <T> T asTemporal(String pattern, TemporalQuery<T> query) {
-    return DateTimeFormatter.ofPattern(pattern).parse(asString(), query);
+  default <T extends Enum<T>> Optional<T> identify(Class<T> enumType) {
+    try {
+      return map(String::toUpperCase)
+        .map(input -> input.replaceAll("([- .])", "_"))
+        .map(input -> Enum.valueOf(enumType, input));
+    } catch (IllegalArgumentException e) {
+      return Optional.empty();
+    }
   }
 
   @JsonCreator
   static InputValue of(String input) {
     return () -> input;
+  }
+
+  static InputValue of(Supplier<String> input) {
+    return input::get;
   }
 
 }
